c3fb603d2790cc9b1dc341212826db0b26ae3dd8
kok <e0332767@u.nus.edu>
2020-02-06 18:13:29 +0800

    Add function to change name

--- client/src/App.js
+++ client/src/App.js
@@ -8,14 +8,22 @@ class App extends Component {
         super();
         this.state = {
             jokes: [],
-            number: 5
+            number: 5,
+            firstName: "Chuck",
+            lastName: "Norris",
         };
 
         this.jokeNumHandler = this.jokeNumHandler.bind(this);
     }
 
     componentWillMount() {
-        fetch('http://api.icndb.com/jokes/random/' + this.state.number)
+        //fetch('http://api.icndb.com/jokes/random/' + this.state.number)
+        fetch('http://api.icndb.com/jokes/random/'
+            + this.state.number
+            + '?firstName='
+            + this.state.firstName
+            + '&lastName='
+            + this.state.lastName)
             .then(result => result.json())
             .then(result => result.value)
             .then(result => result.map(data => data.joke))
@@ -27,12 +35,38 @@ class App extends Component {
     };
 
     generateHandler = event => {
-        fetch('http://api.icndb.com/jokes/random/' + this.state.number)
+        //fetch('http://api.icndb.com/jokes/random/' + this.state.number)
+        fetch('http://api.icndb.com/jokes/random/'
+            + this.state.number
+            + '?firstName='
+            + this.state.firstName
+            + '&lastName='
+            + this.state.lastName)
             .then(result => result.json())
             .then(result => result.value)
             .then(result => result.map(data => data.joke))
             .then(data => this.setState({jokes: data}))
 
+    };
+
+    firstNameHandler = event => {
+        this.setState({
+            firstName: event.target.value
+        })
+    }
+
+    lastNameHandler = event => {
+        this.setState({
+            lastName: event.target.value
+        })
+    }
+
+    vanillaHandler = event => {
+        this.setState({
+            firstName: "Chuck",
+            lastName: "Norris",
+            number: 1
+        }).then(() => this.generateHandler());
     };
 
     render() {
@@ -50,11 +84,19 @@ class App extends Component {
                 <h1> Kok's Chuck Norris App</h1>
                 <p> This app is so badass it made Chuck Norris cry like a little baby.</p>
 
+                <p> First name:</p>
+                <input type="text" onChange={this.firstNameHandler} placeholder="e.g. Chuck" />
+                <p>Last name:</p>
+                <input type="text" onChange={this.lastNameHandler} placeholder="e.g. Norris" />
+
                 <p> Now, how many Chuck Norris Jokes would you like to generate? </p>
-                <input type='text' id="numInput" onChange={this.jokeNumHandler}/>
+                <input type='text' id="numInput" onChange={this.jokeNumHandler} placeholder="e.g. 1"/>
+                <br/>
                 <button onClick={this.generateHandler}>Generate</button>
 
-            <hr />
+                <p onClick={this.vanillaHandler}>Just give me a vanilla Chuck Norris Joke!</p>
+
+                <hr />
 
                 <p>{this.state.number}</p>
                 {jokes}
